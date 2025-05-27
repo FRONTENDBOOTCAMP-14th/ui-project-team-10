36b8fa9cc48679a0c6f3bb0ee9e34ab9b7d792cf
Jeong-il Suk <wjddlf211@naver.com>
2025-05-27 15:14:40 +0900

    quality: 포매팅 쌍따옴표로 통일

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,25 +1,25 @@
 module.exports = {
-  arrowParens: 'always',
-  htmlWhitespaceSensitivity: 'css',
+  arrowParens: "always",
+  htmlWhitespaceSensitivity: "css",
   bracketSameLine: false,
   bracketSpacing: true,
   printWidth: 80,
-  proseWrap: 'preserve',
-  quoteProps: 'as-needed',
+  proseWrap: "preserve",
+  quoteProps: "as-needed",
   semi: true,
   tabWidth: 2,
-  trailingComma: 'es5',
+  trailingComma: "es5",
   useTabs: false,
   overrides: [
     {
-      files: ['*.html'],
+      files: ["*.html"],
       options: {
         tabWidth: 2,
         useTabs: false,
       },
     },
     {
-      files: ['*.css'],
+      files: ["*.css"],
       options: {
         tabWidth: 2,
         useTabs: false,
